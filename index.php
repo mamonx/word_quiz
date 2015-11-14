9e453f31387dee10197d7a4995f70bcc18e8fd03
mamonx <metalmacho11@hotmail.com>
2015-11-15 02:11:00 +0900

    aaaaa

--- index.php
+++ index.php
@@ -6,5 +6,131 @@
 </head>
 <body>
 <!-- ここに頑張って あの単語クイズに似た HTMLを記述してください -->
+<form action="index.php" method="post">
+    <br>
+    <Table align="center" border  Width="300" Height="50">
+        <Tr>
+            <Td width="30%" Align="left">
+                central
+            </Td>
+            <Td width="70%" Align="center">
+                <input type="radio" name="select1" value="1" >不完全な<br>
+                <input type="radio" name="select1" value="2">脈絡のない<br>
+                <input type="radio" name="select1" value="3">中心の
+            </Td>
+        </Tr>
+        </table>
+    <Table align="center" border  Width="300" Height="50">
+        <Tr>
+            <Td width="30%" Align="left">
+                taxation
+            </Td>
+            <Td width="70%" Align="center">
+                <input type="radio" name="select2" value="1" >大胆不敵さ<br>
+                <input type="radio" name="select2" value="2">課税<br>
+                <input type="radio" name="select2" value="3">穴
+            </Td>
+        </Tr>
+    </table>
+    <Table align="center" border  Width="300" Height="50">
+        <Tr>
+            <Td width="30%" Align="left">
+                cosmic
+            </Td>
+            <Td width="70%" Align="center">
+                <input type="radio" name="select3" value="1" >古代の<br>
+                <input type="radio" name="select3" value="2">宇宙の<br>
+                <input type="radio" name="select3" value="3">法人組織の
+            </Td>
+        </Tr>
+    </table>
+    <Table align="center" border  Width="300" Height="50">
+        <Tr>
+            <Td width="30%" Align="left">
+                airy
+            </Td>
+            <Td width="70%" Align="center">
+                <input type="radio" name="select4" value="1" >能力がある<br>
+                <input type="radio" name="select4" value="2">風通しのよい<br>
+                <input type="radio" name="select4" value="3">予期しない
+            </Td>
+        </Tr>
+    </table>
+    <Table align="center" border  Width="300" Height="50">
+        <Tr>
+            <Td width="30%" Align="left">
+                residency
+            </Td>
+            <Td width="70%" Align="center">
+                <input type="radio" name="select5" value="1" >公邸<br>
+                <input type="radio" name="select5" value="2">宇宙<br>
+                <input type="radio" name="select5" value="3">詳細目録
+            </Td>
+        </Tr>
+    </table>
+    <Table align="center" border  Width="300" Height="50">
+        <Tr>
+            <Td width="30%" Align="left">
+                aspiration
+            </Td>
+            <Td width="70%" Align="center">
+                <input type="radio" name="select6" value="1" >歩道<br>
+                <input type="radio" name="select6" value="2">衝突<br>
+                <input type="radio" name="select6" value="3">強い願望
+            </Td>
+        </Tr>
+    </table>
+    <Table align="center" border  Width="300" Height="50">
+        <Tr>
+            <Td width="30%" Align="left">
+                mentor
+            </Td>
+            <Td width="70%" Align="center">
+                <input type="radio" name="select7" value="1" >承認<br>
+                <input type="radio" name="select7" value="2">指導者<br>
+                <input type="radio" name="select7" value="3">手首
+            </Td>
+        </Tr>
+    </table>
+    <Table align="center" border  Width="300" Height="50">
+        <Tr>
+            <Td width="30%" Align="left">
+                stagnation
+            </Td>
+            <Td width="70%" Align="center">
+                <input type="radio" name="select8" value="1" >肥沃<br>
+                <input type="radio" name="select8" value="2">苦痛<br>
+                <input type="radio" name="select8" value="3">沈滞
+            </Td>
+        </Tr>
+    </table>
+    <Table align="center" border  Width="300" Height="50">
+        <Tr>
+            <Td width="30%" Align="left">
+                martial
+            </Td>
+            <Td width="70%" Align="center">
+                <input type="radio" name="select9" value="1" >熱の<br>
+                <input type="radio" name="select9" value="2">戦争の<br>
+                <input type="radio" name="select9" value="3">残りの
+            </Td>
+        </Tr>
+    </table>
+    <Table align="center" border  Width="300" Height="50">
+        <Tr>
+            <Td width="30%" Align="left">
+                gallows
+            </Td>
+            <Td width="70%" Align="center">
+                <input type="radio" name="select10" value="1" >医師<br>
+                <input type="radio" name="select10" value="2">同胞<br>
+                <input type="radio" name="select10" value="3">絞首台
+            </Td>
+        </Tr>
+    </table>
+        <p align="center">
+        <input type="submit" value="送信する">
+    </p>
+</form>
 </body>
 </html>
