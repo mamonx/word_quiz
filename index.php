cb88296d8b44fca71e97e4a8b5de1fc2489c01e7
mamonx <metalmacho11@hotmail.com>
2015-11-15 13:53:18 +0900

    aasd

--- index.php
+++ index.php
@@ -3,9 +3,39 @@
 <html lang="ja">
 <head>
     <meta charset="UTF-8">
+    <link rel="stylesheet" href="./assets/css/bootstrap.min.css">
+    <script src="./assets/js/bootstrap.min.js"></script>
     <title>単語クイズ</title>
 </head>
 <body>
+<form action="result.php" method="post">
+    <?php $quiz->each(function($q) { ?>
+    <?php /** @var \App\System\QuizEntity $q */ ?>
+<div class="container">
+<div class="row">
+    <div class="col-md-12">
+        <table class="table">
+            <tbody>
+            <tr>
+                <th><?= $q->getSubject() ?></th>
+                <td>
+                    <?php foreach($q->getChoices() as $choiceId => $choice): ?>
+                        <p class="text-center">
+                            <input type="radio" name="Answer[<?= $q->getId() ?>]" value="<?= $choiceId ?>" >
+                            <?= htmlspecialchars($choice) ?>
+                        </p>
+                    <?php endforeach ?>
+                </td>
+            </tr>
+            </tbody>
+        </table>
+    </div>
+</div>
+</div>
+    <?php }); ?>
+    <p align="center">
+        <input type="submit" value="送信する">
+    </p>
 <!-- ここに頑張って あの単語クイズに似た HTMLを記述してください -->
 <form action="result.php" method="post">
     <br>
