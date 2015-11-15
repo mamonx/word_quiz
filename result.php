7336936ff3d9677c8bf6cb2103346322801aaede
mamonx <metalmacho11@hotmail.com>
2015-11-15 15:44:02 +0900

    asd3

--- result.php
+++ result.php
@@ -15,26 +15,42 @@ $answers = isset($_POST['Answer']) ? $_POST['Answer'] : [];
 <html lang="ja">
 <head>
     <meta charset="UTF-8">
+    <link rel="stylesheet" href="./assets/css/bootstrap.min.css">
+    <script src="./assets/js/bootstrap.min.js"></script>
     <title>回答結果</title>
 </head>
 <body>
-<?php if ($quiz->judge($answers)): ?>
-    <?php $quiz->each(function($q) use ($answers){ ?>
-        <?php /** @var \App\System\QuizEntity $q */ ?>
-        <table align="center" border  Width="300">
+<div class="container">
+    <div class="row">
+        <div class="col-md-12">
+            <h2>回答結果</h2>
+            <hr/>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-md-12">
+        <table class="table table-bordered">
+            <tbody>
+            <?php if ($quiz->judge($answers)): ?>
+            <?php $quiz->each(function($q) use ($answers){ ?>
+            <?php /** @var \App\System\QuizEntity $q */ ?>
             <tr>
-                <td width="10%" align="center">
+                <td>
                     <?= $q->getAnswer() === (int)$answers[$q->getId()] ? '正解' : '不正解' ?>
                 </td>
-                <td width="30%" align="left">
+                <th>
                     <?= $q->getSubject() ?>
-                </td>
-                <td width="60%" align="center">
+                </th>
+                <td>
                     <?= $q->getChoice($q->getAnswer()) ?>
                 </td>
             </tr>
+            <?php }); ?>
+            </tbody>
         </table>
-    <?php }); ?>
+        </div>
+    </div>
+</div>
 <?php else: ?>
     <?php foreach($quiz->errors() as $number => $error): ?>
         <p style="color: red">
